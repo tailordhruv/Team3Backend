7e7f0a5542e6aabdd44dbd8973b55e2fd53fc8ca
Dhruv Tailor <dhruv.tailor@coviam.com>
2020-01-19 14:03:51 +0530

    gatewayMS commit

---
+++ src/main/java/com/coviam/team3bookstorebackend/Team3bookstorebackendApplication.java
@@ -0,0 +1,13 @@
+package com.coviam.team3bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3bookstorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3bookstorebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coviam/team3bookstorebackend/Team3bookstorebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coviam.team3bookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3bookstorebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
